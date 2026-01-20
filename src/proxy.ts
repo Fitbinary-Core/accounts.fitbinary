0f7e5cc3a8a29af7e4acd447fa703ab950832863
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2026-01-20 12:12:29 +0545

    proxy

---
+++ src/proxy.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const refreshToken = request.cookies.get("refresh_token")?.value;
+  const { pathname } = request.nextUrl;
+
+  const publicPaths = ["/signin", "/signup"];
+
+  if (!refreshToken) {
+    if (publicPaths.includes(pathname)) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  if (refreshToken && publicPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
